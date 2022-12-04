f34a0440adc2b130364cf1ec4814c584a974d617
Ray Zana <rayzana280@gmail.com>
2022-12-03 20:29:39 -0500

    commit tree cc6be055ff88f83028902d5bbfac1b872bfe04bc

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+
+console.log(increment(807));
+console.log(decrement(808));
+
+function makeInt(string){
+   return parseInt(string, 10);
+}
+function preserveDecimal(string){
+   return parseFloat(string);
+}
